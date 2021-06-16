93376086833ecc21e3d58d7a22bdc483011978a6
gustavo177 <39227407+gustavo177@users.noreply.github.com>
2021-06-16 09:49:28 -0500

    Mi aporte es Hola Mundo actualizado en el archivo PjtRepositorio2.java

--- src/pjtrepositorio2/PjtRepositorio2.java
+++ src/pjtrepositorio2/PjtRepositorio2.java
@@ -17,6 +17,7 @@ public class PjtRepositorio2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo como estan");
+        System.out.println("Hola mundo actualizado");
     }
     
 }
